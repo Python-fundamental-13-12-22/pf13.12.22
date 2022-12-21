cf94eca282364935901eddbdc9c45bee681767cf
Andrii Bondarenko <taurussilver777@gmail.com>
2022-12-21 15:49:10 +0200

    add file task02

---
+++ lesson03/hw/Andreyy96/task02.py
@@ -0,0 +1,27 @@
+#2.1 ?
+numbers = 4892
+
+dobutok_numbers =4*8*9*2
+print(dobutok_numbers)
+
+#2.2
+str_number = str(numbers)
+
+revers = "".join(reversed(str_number))
+print(revers)
+print(type(revers))
+
+revers = int(revers)
+print(revers)
+print(type(revers))
+
+
+
+#2.3
+str_number = "".join(sorted(str_number))
+print(str_number)
+print(type(str_number))
+
+int_number = int(str_number)
+print(int_number)
+print(type(int_number))
