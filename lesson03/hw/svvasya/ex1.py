ea4619e014697f66d66b6b366d651b6ab6e6025b
svvasya <svidrykvasyl@gmail.com>
2022-12-22 10:49:20 +0200

    add resolved ex1

--- lesson03/hw/svvasya/ex1.py
+++ lesson03/hw/svvasya/ex1.py
@@ -19,4 +19,10 @@ If the implementation is hard to explain, it's a bad idea.
 If the implementation is easy to explain, it may be a good idea.
 Namespaces are one honking great idea -- let's do more of those!"""
 
-print(str)
+print('better ', str.count("better"),'раз')
+print('never ', str.count("never"),'рази')
+print('is ', str.count("is"),'раз')
+print('-----')
+print('Верхній регістр ', str.upper())
+print('-----')
+print(str.replace('i','&'))
