ed715fb3c7e2aa944a32623ecd7cbd3fbd14936c
art-str <strelnikov-a@ukr.net>
2022-12-23 02:29:54 +0200

    lesson 03 homework

---
+++ lesson03/hw/art-str/task01.py
@@ -0,0 +1,55 @@
+# TASK 1
+# task 1.1
+zen = """The Zen of Python, by Tim Peters
+Beautiful is better than ugly.
+Explicit is better than implicit.
+Simple is better than complex.
+Complex is better than complicated.
+Flat is better than nested.
+Sparse is better than dense.
+Readability counts.
+Special cases aren't special enough to break the rules.
+Although practicality beats purity.
+Errors should never pass silently.
+Unless explicitly silenced.
+In the face of ambiguity, refuse the temptation to guess.
+There should be one-- and preferably only one --obvious way to do it.
+Although that way may not be obvious at first unless you're Dutch.
+Now is better than never.
+Although never is often better than *right* now.
+If the implementation is hard to explain, it's a bad idea.
+If the implementation is easy to explain, it may be a good idea.
+Namespaces are one honking great idea -- let's do more of those!"""
+
+print(zen.count('better'))
+print(zen.count('never'))
+print(zen.count('is'))
+
+# task 1.2
+print(zen.upper())
+
+# task 1.3
+print(zen.replace('i', '&'))
+
+# TASK 2
+# task 2.1
+number = 3452
+multiplication = 3*4*5*2
+print(multiplication)
+
+# task 2.2
+number = 3452
+print(str(number)[::-1])
+
+# task 2.3
+a1 = 3
+a2 = 4
+a3 = 5
+a4 = 2
+print(f"Sorted numbers: {' '.join(sorted([str(a1),str(a2),str(a3),str(a4)]))}")
+
+# TASK 3
+a = 1
+b = 2
+a,b = b,a
+print(b,a)
