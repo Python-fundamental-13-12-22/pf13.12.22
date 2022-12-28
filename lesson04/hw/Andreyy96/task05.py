dd4f2bdb19816930de9e16548f1b96498e1427f2
Andrii Bondarenko <taurussilver777@gmail.com>
2022-12-28 16:38:08 +0200

    add task5

---
+++ lesson04/hw/Andreyy96/task05.py
@@ -0,0 +1,19 @@
+k = int(input("Введите число от 1 до 365: "))
+n = 5
+
+if (k - 1) % 7 < n:
+    k = k % 10
+    if k == 1:
+        print('Понедельник')
+    elif k == 2:
+        print('Вторник')
+    elif k == 3:
+        print('Среда')
+    elif k == 4:
+        print('Четверг')
+    elif k == 5:
+        print('Пятница')
+    elif k == 6:
+        print('Субота')
+    elif k == 7:
+        print('Воскресение')
