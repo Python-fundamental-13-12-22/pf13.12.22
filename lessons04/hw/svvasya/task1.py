c8a2dad060e0bac63978691468ad7c0137d8e40a
svvasya <102587970+svvasya@users.noreply.github.com>
2022-12-29 23:15:35 +0200

    Delete lessons04/hw/svvasya directory

--- lessons04/hw/svvasya/task1.py
+++
@@ -1,8 +0,0 @@
-# ###1
-year = int(input('Введіть рік   '))
-if year%4 == 0 and year%100 != 0:
-    print('''It's a leap year''' )
-elif year % 400 == 0:
-    print('''It's a leap year''')
-else:
-    print('''It's not a leap year''')
--- lessons04/hw/svvasya/task9.py
+++
@@ -1,9 +0,0 @@
-# ##9
-
-n = int(input('введіть n '))
-m = int(input('введіть m '))
-for j in range (1,m):
-    if j == 1 or j == m-1:
-        print("+".center(2)*n)
-    else:
-        print("+", "-".center(2)*(n-2), "+")
