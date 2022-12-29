3a0fbaf3cdbdfc6f9e2a43a476646476966cdd0f
Andrii Bondarenko <taurussilver777@gmail.com>
2022-12-29 12:06:09 +0200

    fix task2

--- lesson04/hw/Andreyy96/task02.py
+++ lesson04/hw/Andreyy96/task02.py
@@ -8,5 +8,9 @@ elif month <= 0 or month > 12:
     print("Некорректный ввод данных: месяц")
 elif day <= 0 or day > 31:
     print("Некорректный ввод данных: день")
+elif month == 2 and day > 28:
+    print("Некорректный ввод данных: день")
+elif (month == 4, 6, 9, 11) and day > 30:
+    print("Некорректный ввод данных: день")
 else:
     print(f'Введено корректные данные: {year}.{month}.{day}')
