10c9f59c207f40b21b8b8e17bd268c58053f71ea
KolyaHarasiuk <kolya.harasiuk@gmail.com>
2023-01-11 15:53:31 +0200

    fix task3 in hw07/harasiuk

--- lesson07/hw/harasiuk/task3.py
+++ lesson07/hw/harasiuk/task3.py
@@ -2,10 +2,17 @@ class Name:
     def __init__(self, first_name, last_name):
         self.fname = first_name
         self.lname = last_name
-        self.fullname = [first_name, last_name]
-        self.initials = first_name[0] + "." + last_name[0]
+
+    def fullname(self):
+        return self.fname, self.lname
+
+    def initials(self):
+        return self.fname[0] + "." + self.lname[0]
 
 
 me = Name("Kolya", "Harasiuk")
-print(me.fullname)
-print(me.initials)
+fullname = me.fullname()
+print(fullname)
+email = me.initials()
+print(email)
+
