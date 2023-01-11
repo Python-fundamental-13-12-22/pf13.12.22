8d4ac32d122a3b369aec52a95fb6b3ca6115ff41
Andrii Bondarenko <taurussilver777@gmail.com>
2023-01-11 16:04:33 +0200

    fix task03

--- lesson07/hw/Andreyy96/task03.py
+++ lesson07/hw/Andreyy96/task03.py
@@ -3,16 +3,18 @@ class Name:
     def __init__(self, first_name, last_name):
         self.fname = first_name
         self.lname = last_name
-        self.fullname = self.fname + ' ' + self.lname
-        self.initials = self.fname[0] + '.' + self.lname[0]
 
+    def fullname(self):
+        return self.fname + ' ' + self.lname
 
-
-
+    def initials(self):
+        return self.fname[0] + '.' + self.lname[0]
 
 first_name = 'Андрей'
 last_name = 'Бондаренко'
 
 person = Name(first_name, last_name)
-print(person.fullname)
-print(person.initials)
+fullname = person.fullname()
+initials = person.initials()
+print(fullname)
+print(initials)
