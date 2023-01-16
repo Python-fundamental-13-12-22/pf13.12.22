58ce146e9381a08f209ca7b70301f6a51fa1b6ea
Andrii Bondarenko <taurussilver777@gmail.com>
2023-01-16 03:23:18 +0200

    fix  additional task03 : Warrior

--- lesson08/hw/Andreyy96/additional task03.py
+++ lesson08/hw/Andreyy96/additional task03.py
@@ -1,33 +1,33 @@
 class Warrior:
 
-    def __init__(self, level=3, rank='Pushover', experoence=100, achievements=[]):
+    def __init__(self, level=1, rank='Pushover', experoence=100, achievements=[]):
         self.level = level
         self.rank = rank
         self.experience = experoence
         self.achievements = achievements
 
     def rank_warrior(self):
-        if self.level >= 1 and self.level <= 9 :
+        if 1 <= self.level <= 9:
             self.rank = 'Pushover'
-        elif self.level > 9 and self.level <= 19:
+        elif 9 < self.level <= 19:
             self.rank = 'Novice'
-        elif self.level > 19 and self.level <= 29:
+        elif 19 < self.level <= 29:
             self.rank = 'Fighter'
-        elif self.level > 29 and self.level <= 39:
+        elif 29 < self.level <= 39:
             self.rank = 'Warrior'
-        elif self.level > 39 and self.level <= 49:
+        elif 39 < self.level <= 49:
             self.rank = 'Veteran'
-        elif self.level > 49 and self.level <= 59:
+        elif 49 < self.level <= 59:
             self.rank = 'Sage'
-        elif self.level > 59 and self.level <= 69:
+        elif 59 < self.level <= 69:
             self.rank = 'Elite'
-        elif self.level > 69 and self.level <= 79:
+        elif 69 < self.level <= 79:
             self.rank = 'Conqueror'
-        elif self.level > 79 and self.level <= 89:
+        elif 79 < self.level <= 89:
             self.rank = 'Champion'
-        elif self.level > 89 and self.level <= 99:
+        elif 89 < self.level <= 99:
             self.rank = 'Master'
-        elif self.level > 99 and self.level == 100:
+        elif 99 < self.level == 100:
             self.rank = 'Greatest'
         else:
             if self.level < 1:
@@ -39,30 +39,26 @@ class Warrior:
                 self.rank = 'Greatest'
                 self.experience = 10000
 
-
     def battle_rules(self, enemy_level):
         message = ''
         if self.level < 1 or self.level > 100:
             return 'Invalid level'
         elif self.level == enemy_level:
             self.experience += 10
-            self.rank_warrior()
         elif self.level - enemy_level == 1:
             self.experience += 5
-            self.rank_warrior()
         elif self.level - enemy_level == 2:
             self.experience += 0
-            self.formula_get_level(enemy_level)
-            self.rank_warrior()
         elif enemy_level - self.level == 6:
             self.formula_get_level(enemy_level)
-        elif enemy_level - self.level >= 1 and enemy_level - self.level < 5:
+        elif 1 <= enemy_level - self.level < 5:
             self.formula_get_level(enemy_level)
         elif self.level - enemy_level <= -5:
             print("You've been defeated")
+        self.rank_warrior()
 
         if self.level - enemy_level >= 2:
-            message  = "Easy fight"
+            message = "Easy fight"
         elif self.level - enemy_level == 1 or self.level == enemy_level:
             message = "A good fight"
         elif self.level < enemy_level:
@@ -84,7 +80,6 @@ class Warrior:
         else:
             return "Not strong enough"
         return trainer[0]
-
 
 jet_li = Warrior()
 
