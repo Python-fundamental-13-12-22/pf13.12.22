97c3ce7178b0cd15c839870bef537ac60a16b6a6
svvasya <102587970+svvasya@users.noreply.github.com>
2023-01-17 17:26:52 +0200

    Update task1.py

--- lesson07/hw/svvasya/task1.py
+++ lesson07/hw/svvasya/task1.py
@@ -1,21 +1,21 @@
 class Person():
-    my_age = 36
+    #my_age = 36
     def __init__(self, name=None, age=None):
         #print(self, self.__dict__)
         self.name = name
         self.age = age
         #print(self, self.__dict__)
-    def compare_age(self):
-        if self.age > self.my_age:
-            return f"""{self.name} is older than Me"""
-        elif self.age < self.my_age:
-            return f"""{self.name} is younger than Me"""
-        elif self.age == self.my_age:
-            return f"""{self.name} is the same age as Me"""
+    def compare_age(self,person):
+        if self.age > person.age:
+            return f"""{self.name} is older than {person.name}"""
+        elif self.age < person.age:
+            return f"""{self.name} is younger than {person.name}"""
+        elif self.age == person.name.age:
+            return f"""{self.name} is the same age as {person.name}"""
 
 p1 = Person('Тарас', 40)
 p2 = Person('Володимир', 35)
 p3 = Person('Іван', 36)
-print(p1.compare_age())
-print(p2.compare_age())
-print(p3.compare_age())
+print(p1.compare_age(p2))
+print(p2.compare_age(p1))
+print(p3.compare_age(p2))
