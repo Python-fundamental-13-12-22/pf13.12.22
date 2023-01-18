2da5e751c0888829480165dce11d51a80cbef651
svvasya <svidrykvasyl@gmail.com>
2023-01-18 23:04:55 +0200

    add day of week

--- lesson09/hw/svvasya/class_check_email.py
+++ lesson09/hw/svvasya/class_check_email.py
@@ -1,12 +1,12 @@
 import string
 class CheckEmail:
-    def __init__(self, string_email= ''):
-        self.string_email = string_email
-    def valid_email(self):
-        self.string_email.split('@')
+    #def __init__(self, string_email= ''):
+    #    self.string_email = string_email
+    def valid_email(self, string_email = ''):
+        string_email.split('@')
         try:
-            user_info = self.string_email.split('@')[0]
-            domain_info = self.string_email.split('@')[1]
+            user_info = string_email.split('@')[0]
+            domain_info = string_email.split('@')[1]
             email_dict = dict.fromkeys(string.ascii_lowercase, 0)
             email_dict['.'] = '0'
             email_dict = tuple(email_dict)
@@ -26,14 +26,14 @@ class CheckEmail:
 
 
 input_email = str(input('Please enter your email '))
-email0 = CheckEmail(input_email)
-print(email0.valid_email())
+email0 = CheckEmail()
+print(email0.valid_email(input_email))
 input_email = str(input('Please enter your email '))
-email1 = CheckEmail(input_email)
-print(email1.valid_email())
+email1 = CheckEmail()
+print(email1.valid_email(input_email))
 input_email = str(input('Please enter your email '))
-email2 = CheckEmail(input_email)
-print(email2.valid_email())
+email2 = CheckEmail()
+print(email2.valid_email(input_email))
 input_email = str(input('Please enter your email '))
-email3 = CheckEmail(input_email)
-print(email3.valid_email())
+email3 = CheckEmail()
+print(email3.valid_email(input_email))
