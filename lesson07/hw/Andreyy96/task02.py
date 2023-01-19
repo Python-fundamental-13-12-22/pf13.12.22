28958eb7f1d28a76d96788d9e8758f51a5d1cee4
Andrii Bondarenko <taurussilver777@gmail.com>
2023-01-19 16:27:21 +0200

    fix task02

--- lesson07/hw/Andreyy96/task02.py
+++ lesson07/hw/Andreyy96/task02.py
@@ -3,13 +3,18 @@ class Employee:
     def __init__(self, first_name, last_name):
         self.first_name = first_name
         self.last_name = last_name
-        self.fullname = first_name + ' ' + last_name
-        self.email = first_name.lower() + '.' + last_name.lower() + "@company.com"
+
+    def fullname(self):
+        fullname_person = self.first_name + " " + self.last_name
+
+        return fullname_person
+
+    def email(self):
+        email_person = self.first_name.lower() + "." + self.last_name.lower() + "@company.com"
+
+        return email_person
 
 
-first_name = 'Андрей'
-last_name = 'Бондаренко'
-
-person = Employee(first_name, last_name)
-print(person.fullname)
-print(person.email)
+person = Employee('Andrii', 'Bondarenko')
+print(person.fullname())
+print(person.email())
