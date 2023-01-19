eee8063cefb49612df7fa3871e1e0cd0289cdba3
Andrii Bondarenko <taurussilver777@gmail.com>
2023-01-19 16:27:11 +0200

    fix task01

--- lesson07/hw/Andreyy96/task01.py
+++ lesson07/hw/Andreyy96/task01.py
@@ -3,21 +3,19 @@ class Person:
     def __init__(self, name, age):
         self.name = name
         self.age = age
-        self.my_age = 26
 
-    def compare_age(self):
-        if self.age < self.my_age:
-            print(f'{self.name}: {self.age} is younger than me {self.my_age}')
-        elif self.age > self.my_age:
-            print(f'{self.name}: {self.age} is older than me {self.my_age}')
+    def compare_age(self, other_person):
+        if self.age < other_person.age:
+            print(f'{self.name}: is younger than me')
+        elif self.age > other_person.age:
+            print(f'{self.name} is older than me')
         else:
-            print(f'{self.name}: {self.age} is the same age as me {self.my_age}')
+            print(f'{self.name} is the same age as me')
 
 p1 = Person('Дэн', 26)
-p1.compare_age()
-
 p2 = Person('Дання', 19)
-p2.compare_age()
-
 p3 = Person('Дианна', 30)
-p3.compare_age()
+p1.compare_age(p2)
+p2.compare_age(p3)
+p3.compare_age(p1)
+p3.compare_age(p3)
