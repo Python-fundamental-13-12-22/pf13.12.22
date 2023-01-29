cdeedc8bd3dbeed03cface81875fa443a35b2385
svvasya <svidrykvasyl@gmail.com>
2023-01-29 15:24:50 +0200

    fix task2

--- lesson10/hw/svvasya/task2.py
+++ lesson10/hw/svvasya/task2.py
@@ -23,16 +23,10 @@ class Table:
     @price.setter
     def price(self, price):
         self._price = price
-
-    def is_table(self,cell):
-        if cell == 'стіл':
-            return True
-
-class Chairs:
+class Chair:
     def __init__(self, material="", price=0):
         self._material = material
         self._price = price
-
     def __repr__(self):
         return f"({self._material},{self._price})"
 
@@ -51,28 +45,20 @@ class Chairs:
     @price.setter
     def price(self, price):
         self._price = price
-    def is_chair(self,cell):
-        if cell == 'стілець':
-            return True
-
-class Nabir_mebliv(Chairs,Table):
-    def __init__(self, name="", table="", chair="", count_chair = 0 ):
+class Nabir_mebliv(Chair,Table):
+    def __init__(self, name="", tabl="", chair="", count_chair = 0 ):
         self.name = name
-        self.table = table
+        self.tabl = tabl
         self.chair = chair
         self.count_chair =  count_chair
 
     def __repr__(self):
-        return f"({self.name},{self.table}, {self.chair}, {self.count_chair})"
-    def create_nabir(self,size , count):
+        return f"({self.name},{self.tabl}, {self.chair}, {self.count_chair})"
+    def create_nabir(self,size , count, material):
         self.size = size
         self.count_chair = count
-        return [size, count]
-
-
-
-
-
+        self.material = material
+        return [size, count, material]
 
 
 inputFile = open("mebli_input.txt", "r")
@@ -81,28 +67,44 @@ for line in inputFile:
     s_i = line.encode('cp1251').decode('utf8').split(',')
     s.append(s_i)
 inputFile.close()
-print(s)
-
-chair = Chairs()
-table = Table()
-nabir = Nabir_mebliv()
-
-k = 0
-select_nabir = nabir.create_nabir('2х1м',4)
+chairs_ar = []
+tables_ar = []
 for i in range(0,len(s)):
-    if s[i][2] == select_nabir[0] and  table.is_table(s[i][0]):
-        nabir.name = 'Набір ' + str(s[i][0]) + ' ' + select_nabir[0] + ' ----- '
-        nabir.table = ' ' + str(s[i][0]) +  ' ' + str(s[i][1]) +  ' ' + select_nabir[0] + ' ----- '
-    if chair.is_chair(s[i][0]):
+    type_m = s[i][0]
+    material = s[i][1]
+    if type_m == 'стілець':
+        price= s[i][2]
+        chairs = Chair(material=material,
+                        price=price,
+                           )
+        chairs_ar.append(chairs)
+    elif type_m == 'стіл':
+        price= s[i][3]
+        size = s[i][2]
+        tables = Table(size=size,
+                      material=material,
+                      price=price)
+        tables_ar.append(tables)
+nabir = Nabir_mebliv()
+k = 0
+# вхідні дані
+select_nabir = nabir.create_nabir('2х1м',4,'дуб')
+for table in tables_ar:
+    if type(table) is Table and select_nabir[0] == table.size and select_nabir[2] == table.material:
+        nabir.name = 'Набір стіл і стільці '
+        nabir.tabl = 'Стіл' + ' ' + table.material + ' ' + table.size + ' ----- '
+for chairs in chairs_ar:
+    if type(chairs) is Chair and select_nabir[2] == chairs.material:
         k = k + 1
-
 if k >= select_nabir[1]:
-    nabir.chair = "стільці шт.: "
+    nabir.chair = "стільці "+ select_nabir[2] +", шт.: "
     print(nabir)
     with open('mebli_output.txt', 'w') as file:
         file.write(str(nabir).encode('utf8').decode('cp1251'))
         print(f' file mebli_output.txt saved')
 else:
+    with open('mebli_output.txt', 'w') as file:
+        file.write('неможливо скласти набір')
     print(f'неможливо скласти набір ')
 
 
